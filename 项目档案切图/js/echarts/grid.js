e23e40926a3b1df26e101d4a3502338ce02490fd
lizhongyu <13552456834@163.com>
2017-04-05 16:05:56 +0800

    项目一

---
+++ 项目档案切图/js/echarts/grid.js
@@ -0,0 +1,97 @@
+/**
+ * Grid theme for Highcharts JS
+ * @author Torstein Hønsi
+ */
+
+Highcharts.theme = {
+	colors: ['#ED561B', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
+	chart: {
+		backgroundColor: {
+			linearGradient: [0, 0, 500, 500],
+			stops: [
+				[0, 'rgb(255, 255, 255)'],
+				[1, 'rgb(255, 255, 255)']
+			]
+		}
+,
+		borderWidth: 1,
+		plotBackgroundColor: 'rgba(255, 255, 255, .9)',
+		plotShadow: true,
+//		plotBorderWidth: 1
+	},
+	title: {
+		style: { 
+			color: '#FF0000',
+			font: 'bold 24px "Trebuchet MS", Verdana, sans-serif'
+		}
+	},
+	subtitle: {
+		style: { 
+			color: '#666666',
+			font: 'bold 16px "Trebuchet MS", Verdana, sans-serif'
+		}
+	},
+	xAxis: {
+//		gridLineWidth: 1,
+		lineColor: '#000',
+		tickColor: '#000',
+		labels: {
+			style: {
+				color: '#000',
+				font: '11px Trebuchet MS, Verdana, sans-serif'
+			}
+		},
+		title: {
+			style: {
+				color: '#333',
+				fontWeight: 'bold',
+				fontSize: '16px',
+				fontFamily: 'Trebuchet MS, Verdana, sans-serif'
+
+			}				
+		}
+	},
+	yAxis: {
+//		minorTickInterval: 'auto',
+		lineColor: '#000',
+		lineWidth: 1,
+//		tickWidth: 1,
+		tickColor: '#000',
+		labels: {
+			style: {
+				color: '#000',
+				font: '16px Trebuchet MS, Verdana, sans-serif'
+			}
+		},
+		title: {
+			style: {
+				color: '#333',
+				fontWeight: 'bold',
+				fontSize: '16px',
+				fontFamily: 'Trebuchet MS, Verdana, sans-serif'
+			}				
+		}
+	},
+	legend: {
+		itemStyle: {			
+			font: '9pt Trebuchet MS, Verdana, sans-serif',
+			color: 'black'
+
+		},
+		itemHoverStyle: {
+			color: '#039'
+		},
+		itemHiddenStyle: {
+			color: '#fff'
+		}
+	},
+	labels: {
+		style: {
+			color: '#fff'
+		}
+	}
+};
+
+// Apply the theme
+var highchartsOptions = Highcharts.setOptions(Highcharts.theme);
+	
